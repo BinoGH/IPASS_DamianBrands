0d573703ef2ddcff93aabd2badd01364f5232513
Bino <74612564+BinoGH@users.noreply.github.com>
2021-06-25 00:04:13 +0200

    Blob

---
+++ src/main/java/nl/damian/IPASS/persistence/CollectiveClass.java
@@ -0,0 +1,28 @@
+package nl.damian.IPASS.persistence;
+
+import nl.damian.IPASS.domein.Gebruiker;
+import nl.damian.IPASS.domein.Product;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class CollectiveClass implements Serializable {
+    private ArrayList<Product> assortiment = new ArrayList<Product>();
+    private ArrayList<Gebruiker> gebruikers = new ArrayList<>();
+
+    public ArrayList<Product> getAssortiment() {
+        return assortiment;
+    }
+
+    public void setAssortiment(ArrayList<Product> assortiment) {
+        this.assortiment = assortiment;
+    }
+
+    public ArrayList<Gebruiker> getGebruikers() {
+        return gebruikers;
+    }
+
+    public void setGebruikers(ArrayList<Gebruiker> gebruikers) {
+        this.gebruikers = gebruikers;
+    }
+}
